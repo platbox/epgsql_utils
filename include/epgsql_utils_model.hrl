b6190105c2f74a2592a23635b414b8484d5d0a57
Artem Ocheredko <aaocheredko@platbox.com>
2014-04-22 17:05:44 +0400

    Added missed model type declarations

--- include/epgsql_utils_model.hrl
+++ include/epgsql_utils_model.hrl
@@ -4,6 +4,8 @@
 -type key(T) :: T.
 -type date() :: calendar:date().
 -type datetime() :: calendar:datetime().
+-type value () :: 'null' | binary() | boolean() | number() | [value()] | object().
+-type object() :: {[{binary(), value()}]}.
 -type json  () :: [value()] | object().
 
 -endif.
